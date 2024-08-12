8438132d34abaa138704f9d7f09fa255d24ee597
Suraj Singh <92253228+surajsingh0912@users.noreply.github.com>
2024-08-13 00:09:50 +0530

    Add files via upload

---
+++ 02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3); //code readability should be high
+console.log("suraj")
+
+let name = "Suraj"
+let age = 21
+let isLoggedin = false
+
+// number => 2 to power 53 
+// bigint (used rarely when number is too big)
+// string => ""
+//  boolean => true/false
+// null => standalone value
+// undefined => 
+//symbol => unique
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof undefined); //object
