0dbe0c435eb4a6f338f6e8199116de6d28a04a60
Suraj Singh <surajsingh45908@gmail.com>
2024-08-13 23:53:09 +0530

    conversion of data type

--- 03_conversionoperation.js
+++ 03_conversionoperation.js
@@ -1,8 +1,28 @@
-let score = "23abv"
+// let score = "23abv"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// conversions to Number
+// "33" => 33
+// "33abc" => NaN
+// true =>1
+
+// let isLoggedIn = "suraj"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// conversion to boolean
+// 1 => true ; 0 => false
+// "" => false
+// "suraj" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber) // output will be same but data type will be string
+console.log(typeof stringNumber);
