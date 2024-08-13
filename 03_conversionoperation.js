6ae3e525ab15180154e66387ef6ff50131e30c42
Suraj Singh <surajsingh45908@gmail.com>
2024-08-14 00:14:44 +0530

    operations

--- 03_conversionoperation.js
+++ 03_conversionoperation.js
@@ -22,7 +22,45 @@
 // "" => false
 // "suraj" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber) // output will be same but data type will be string
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber) // output will be same but data type will be string
+// console.log(typeof stringNumber);
+
+
+// *************** Operations ***************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = "suraj"
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2  //can be written its not a ethical behavior
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
