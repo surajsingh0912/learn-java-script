71fee0be7b9f696784e2b27141b9ac07162fc42a
Suraj Singh <surajsingh45908@gmail.com>
2024-08-22 03:39:00 +0530

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,60 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol(used to make any value unique), BigInt
+
+// const score = 100
+// const scorevalue =100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log( id === anotherId);
+
+// const bigNumber = 336465462131654613134692532134n // n is used to make it bigint
+// console.log(typeof bigNumber);
+
+// Reference (non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myobj = {
+    name: "suraj",
+    age: 22,
+}
+
+console.log(heros);
+console.log(myobj);
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof heros);
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
+
+
+
+
+
+
+
